9f488e63e5e327fa026f7bb3530b11f59987cbcd
nate <clevelandsun@gmail.com>
2020-05-08 19:38:33 -0400

    Many to Many map Recipe to Category

---
+++ src/main/java/com/FreeCodeUserJack/recipeProject/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.FreeCodeUserJack.recipeProject.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/FreeCodeUserJack/recipeProject/domain/Recipe.java
+++ src/main/java/com/FreeCodeUserJack/recipeProject/domain/Recipe.java
@@ -17,12 +17,41 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // todo add
-    // private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING) // for enums
+    private Difficulty difficulty;
 
     // mappedBy determines the property of child entity that is this parent entity
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @Lob
+    private Byte[] image; // interesting for JPA mapping
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 
     public Set<Ingredient> getIngredients() {
         return ingredients;
@@ -31,12 +60,6 @@ public class Recipe {
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
     }
-
-    @Lob
-    private Byte[] image; // interesting for JPA mapping
-
-    @OneToOne(cascade = CascadeType.ALL)
-    private Notes notes;
 
     public Long getId() {
         return id;
