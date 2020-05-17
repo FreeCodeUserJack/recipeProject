84f2a61ca0abe1ca211b83564ba13b4af7118161
nate <clevelandsun@gmail.com>
2020-05-17 15:11:01 -0400

    added view ingredients

---
+++ src/main/java/com/FreeCodeUserJack/recipeProject/controllers/IngredientController.java
@@ -0,0 +1,30 @@
+package com.FreeCodeUserJack.recipeProject.controllers;
+
+import com.FreeCodeUserJack.recipeProject.Services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String getIngredients(@PathVariable String recipeId, Model model) {
+        log.debug("Showing ingredients for recipe with id: " + recipeId);
+
+        // use command object to avoid lazy load errors in Thymeleaf
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "/recipe/ingredient/list";
+    }
+}
---
+++ src/test/java/com/FreeCodeUserJack/recipeProject/controllers/IngredientControllerTest.java
@@ -0,0 +1,50 @@
+package com.FreeCodeUserJack.recipeProject.controllers;
+
+import com.FreeCodeUserJack.recipeProject.Services.RecipeService;
+import com.FreeCodeUserJack.recipeProject.commands.RecipeCommand;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class IngredientControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController ingredientController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        ingredientController = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+    }
+
+    @Test
+    public void testListIngredients() throws Exception {
+        // given
+        RecipeCommand com = new RecipeCommand();
+        com.setId(2L);
+
+        // when
+        when(recipeService.findCommandById(anyLong())).thenReturn(com);
+
+        // then
+        mockMvc.perform(get("/recipe/1/ingredients"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("/recipe/ingredient/list"))
+            .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
