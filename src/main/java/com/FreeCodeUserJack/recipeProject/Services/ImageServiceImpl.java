b39649e7ca09434c7f9491c950299b8d85121030
nate <clevelandsun@gmail.com>
2020-05-20 18:25:32 -0400

    saved image to DB

--- src/main/java/com/FreeCodeUserJack/recipeProject/Services/ImageServiceImpl.java
+++ src/main/java/com/FreeCodeUserJack/recipeProject/Services/ImageServiceImpl.java
@@ -1,16 +1,47 @@
 package com.FreeCodeUserJack.recipeProject.Services;
 
+import com.FreeCodeUserJack.recipeProject.domain.Recipe;
+import com.FreeCodeUserJack.recipeProject.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService{
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
 
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] image = new Byte[file.getBytes().length]; // setup new Byte array
+
+            int i = 0;
+
+            // copy over byte by byte
+            for (byte b: file.getBytes()) {
+                image[i++] = b;
+            }
+
+            recipe.setImage(image);
+            recipeRepository.save(recipe);
+        }
+        catch(IOException e) {
+            // todo handle error
+            log.error("Error occurred", e);
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/FreeCodeUserJack/recipeProject/Services/ImageServiceImplTest.java
+++ src/test/java/com/FreeCodeUserJack/recipeProject/Services/ImageServiceImplTest.java
@@ -1,10 +1,55 @@
 package com.FreeCodeUserJack.recipeProject.Services;
 
+import com.FreeCodeUserJack.recipeProject.domain.Recipe;
+import com.FreeCodeUserJack.recipeProject.repositories.RecipeRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 public class ImageServiceImplTest {
 
+    @Mock
+    RecipeRepository recipeRepository;
+
+    ImageService imageService;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
     @Test
-    public void saveImageFile() {
+    public void saveImageFile() throws IOException {
+        // given
+        Long id = 1L;
+        MultipartFile file = new MockMultipartFile("imagefile", "test.txt", "text/plain",
+                "Spring Framework Guru".getBytes());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        // when
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        imageService.saveImageFile(id, file);
+
+        // then
+        verify(recipeRepository, times(1)).save(argumentCaptor.capture());
+        Recipe capturedRecipe = argumentCaptor.getValue();
+        assertEquals(file.getBytes().length, capturedRecipe.getImage().length);
     }
 }
